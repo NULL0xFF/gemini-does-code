d1c3bad8eca448ef4658c5102fea8307f902e885
Myoung Ha, Ji <webmaster@null0xff.com>
2026-03-03 03:07:07 +0900

    build(backend): 📦 Add dependencies for OAuth2, JPA, MariaDB, and Swagger

--- backend/server/build.gradle.kts
+++ backend/server/build.gradle.kts
@@ -6,6 +6,13 @@ plugins {
 dependencies {
     implementation(project(":plugin-api"))
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.15")
     implementation("org.pf4j:pf4j-spring:0.9.0")
+    
+    runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
+    runtimeOnly("com.h2database:h2")
+    
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
